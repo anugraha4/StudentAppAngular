a5ecd579faa2d5fdd2b193cfd54d2cd53ec32d78
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-11-28 17:58:41 +0530

    Student Search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
 import { ViewAllComponent } from './view-all/view-all.component';
+import { StudentSearchComponent } from './student-search/student-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentEntryComponent,
-    ViewAllComponent
+    ViewAllComponent,
+    StudentSearchComponent
   ],
   imports: [
     BrowserModule,
